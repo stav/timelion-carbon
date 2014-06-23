403f57b0b6a1ff8bcc66d7a39cb481fdac18d00a
Steven Almeroth <sroth77@gmail.com>
2014-06-22 21:30:33 -0500

    data moved to json files and retrieved via $http service

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
-/*
- * TimeLion - Carbon
- *
+/* TimeLion - Carbon
  * AngularJS implementation of a daily log.
- *
- * To do: relative dosage sizes for columns
  */
 (function() {
   var app = angular.module('TimelionApp', []);
 
-  app.controller('TimeController', function() {
-    this.events = events;
-  });
+  var meds = [];
+
+  app.controller('TimeController', ['$http', function($http) {
+    var time = this;
+    time.events = [];
+
+    $http.get('js/events.json').success(function(data) {
+      time.events = data;
+    });
+  }]);
 
   app.controller('EventController', function() {
-    this._meds = meds;
-
     this.init = function(evnt) {
       this.evnt = evnt;
     };
@@ -37,78 +38,24 @@
       return this.getMed(med)[batch] < 0;
     };
     this.med = function(med, dose) {
-      return '' + Math.abs(dose) + ' ' + this._meds[med].unit;
+      return '' + Math.abs(dose) + ' ' + meds[med].unit;
     };
   });
 
-  app.controller('MedController', function() {
-    this._meds = meds;
+  app.controller('MedController', ['$http', function($http) {
+    $http.get('js/meds.json').success(function(data) {
+      meds = data;
+    });
 
     this.getMeds = function() {
       var medicine_names = [];
 
-      for (var i = 0; i < this._meds.length; i++) {
-        medicine_names.push(this._meds[i].name);
+      for (var i = 0; i < meds.length; i++) {
+        medicine_names.push(meds[i].name);
       };
 
       return medicine_names;
     };
-  });
+  }]);
 
-  var events = [
-    [ '02 June', [ [   0],   [   0     ],   [   0     ],   [   0],   [   0    ],   [  0    ],   [  0    ] ] ],
-    [ '03 June', [ [   0],   [   0     ],   [   0     ],   [   0],   [   0    ],   [  0    ],   [  0    ] ] ],
-    [ '04 June', [ [   0],   [   0     ],   [   0     ],   [   0],   [   0    ],   [  0    ],   [  0    ] ] ],
-    [ '05 June', [ [   0],   [   0     ],   [   0     ],   [   0],   [   0    ],   [  0    ],   [  0    ] ] ],
-    [ '06 June', [ [ 200],   [ 200     ],   [ 375, 375],   [ 4.5],   [ 100    ],   [  0    ],   [ 2, 2  ] ] ],
-    [ '07 June', [ [ 200],   [   0     ],   [ 375, 375],   [ 4.5],   [ 100    ],   [  0    ],   [ 4     ] ] ],
-    [ '08 June', [ [-200],   [   0     ],   [-375,-375],   [-4.5],   [-100    ],   [  0    ],   [  0    ] ] ],
-    [ '09 June', [ [-200],   [   0     ],   [-375, 375],   [-4.5],   [-100    ],   [  0    ],   [ 4     ] ] ],
-    [ '10 June', [ [ 200],   [   0     ],   [ 375, 375],   [ 4.5],   [ 100    ],   [  0    ],   [ 2, 3  ] ] ],
-    [ '11 June', [ [ 200],   [   0     ],   [ 375, 375],   [ 4.5],   [ 100    ],   [  0    ],   [ 3, 2  ] ] ],
-    [ '12 June', [ [ 200],   [   0     ],   [ 375, 375],   [ 4.5],   [ 100    ],   [  0    ],   [ 2, 4  ] ] ],
-    [ '13 June', [ [ 200],   [   0     ],   [ 375, 375],   [ 4.5],   [ 40, 40 ],   [  0    ],   [ 3, 3  ] ] ],
-    [ '14 June', [ [ 200],   [   0     ],   [ 375, 375],   [ 4.5],   [ 60, 40 ],   [  0    ],   [ 4     ] ] ],
-    [ '15 June', [ [ 200],   [   0     ],   [ 375, 375],   [ 4.5],   [ 50, 40 ],   [  0    ],   [ 4     ] ] ],
-    [ '16 June', [ [ 200],   [ 200     ],   [ 375     ],   [ 4.5],   [ 50, 25 ],   [ 20    ],   [ 3     ] ] ],
-    [ '17 June', [ [ 200],   [ 200, 200],   [   0     ],   [ 4.5],   [ 25, 25 ],   [ 20, 20],   [ 4     ] ] ],
-    [ '18 June', [ [ 200],   [ 200, 200],   [   0     ],   [ 4.5],   [ 25, 25 ],   [ 20, 20],   [4,2,2  ] ] ],
-    [ '19 June', [ [ 200],   [ 200, 200],   [   0     ],   [ 4.5],   [ 25, 25 ],   [ 20, 20],   [4,5    ] ] ],
-    [ '20 June', [ [ 200],   [ 200, 200],   [   0     ],   [ 4.5],   [   0    ],   [  0    ],   [4,4,4,2] ] ],
-    [ '21 June', [ [ 200],   [ 200, 200],   [   0     ],   [ 4.5],   [   0    ],   [  0    ],   [4,2    ] ] ],
-    [ 'end'    , [ [   0],   [   0     ],   [   0     ],   [   0],   [   0    ],   [  0    ],   [  0    ] ] ]
-  ]
-
-  var meds = [
-    {
-      name: 'Omeprazol',
-      unit: 'mg'
-    },
-    {
-      name: 'Clindamicina',
-      unit: 'mg'
-    },
-    {
-      name: 'Metronidazol',
-      unit: 'mg'
-    },
-    {
-      name: 'Enrofloxacina',
-      unit: 'ml'
-    },
-    {
-      name: 'Carprofen',
-      note: 'Sold as Rimadyl & Carproby',
-      unit: 'mg'
-    },
-    {
-      name: 'Tramadol',
-      unit: 'mg'
-    },
-    {
-      name: 'Cannabis',
-      unit: 'drops'
-    },
-    {}
-  ]
 })();
