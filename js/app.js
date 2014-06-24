d94fea3867a76d4209b08fd389f261f509451f8e
Steven Almeroth <sroth77@gmail.com>
2014-06-24 12:47:34 -0500

    code cleanups

--- js/app.js
+++ js/app.js
@@ -2,75 +2,77 @@
  * AngularJS implementation of a daily log.
  */
 (function() {
-  angular.module('TimelionCarbonApp', [])
+  angular.
 
-  .factory('EventsDataService', function($http) {
+  module('TimelionCarbonApp', []).
+
+  factory('EventsDataService', function ($http) {
     return $http({method: 'GET', url: 'js/events.json'});
-  })
+  }).
 
-  .factory('MedsDataService', function($http) {
+  factory('MedsDataService', function ($http) {
     return $http({method: 'GET', url: 'js/meds.json'});
-  })
+  }).
 
-  .controller('TimeController', function(EventsDataService) {
+  controller('TimeController', function (EventsDataService) {
     var store = this;
     store.events = [];
 
-    EventsDataService.then(function(result) {
+    EventsDataService.then(function (result) {
       store.events = result.data;
     });
-  })
+  }).
 
-  .controller('EventController', function(MedsDataService) {
+  controller('EventController', function (MedsDataService) {
     var store = this;
     store.meds = [];
 
-    MedsDataService.then(function(result) {
+    MedsDataService.then(function (result) {
       store.meds = result.data;
     });
 
-    this.init = function(evnt) {
-      this.evnt = evnt;
+    this.init = function (event) {
+      this.event = event;
     };
-    this.getMed = function(med) {
-      return this.evnt.meds.length > med ? this.evnt.meds[med] : [];
+    this.getMed = function (med) {
+      return this.event.meds.length > med ? this.event.meds[med] : [];
     };
-    this.getClinicVisitors = function(clinic) {
-      return this.evnt.clinic[clinic].split('');
+    this.getClinicVisitors = function (clinic) {
+      return this.event.clinic[clinic].split('');
     };
-    this.atClinic = function(clinic) {
-      return this.evnt.clinic[clinic].length > 0;
+    this.atClinic = function (clinic) {
+      return this.event.clinic[clinic].length > 0;
     };
-    this.hasMeds = function() {
-      return this.evnt.meds instanceof Array && this.evnt.meds.length > 0;
+    this.hasMeds = function () {
+      return this.event.meds instanceof Array && this.event.meds.length > 0;
     };
-    this.hasDosage = function(med) {
+    this.hasDosage = function (med) {
       return this.getMed(med).length > 0;
     };
-    this.getMedsRange = function(med) {
+    this.getMedsRange = function (med) {
       return Array(Math.abs(this.getMed(med)));
     };
-    this.adminHome = function(med, batch) {
+    this.observed = function (med, batch) {
       return this.getMed(med)[batch] > 0;
     };
-    this.adminClinic = function(med, batch) {
+    this.unobserved = function (med, batch) {
       return this.getMed(med)[batch] < 0;
     };
-    this.med = function(med, dose) {
+    this.med = function (med, dose) {
       if (med in store.meds)
         return '' + Math.abs(dose) + ' ' + store.meds[med].unit;
     };
-  })
+  }).
 
-  .controller('MedController', function(MedsDataService) {
+  controller('MedController', function (MedsDataService) {
     var store = this;
     store.meds = [];
 
-    MedsDataService.then(function(result) {
+    MedsDataService.then(function (result) {
       store.meds = result.data;
     });
 
-    this.getMeds = function() {
+    this.getMeds = function () {
       var medicine_names = [];
 
       for (var i = 0; i < store.meds.length; i++) {
