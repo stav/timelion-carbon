9709fc9851143a833d3a2d3138be3b8f55e4cf32
Steven Almeroth <sroth77@gmail.com>
2015-05-23 00:15:52 -0500

    add X-ray html including new rawhtml filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -10,4 +10,10 @@ filter('interpolate', ['version', function(version) {
   return function(text) {
     return String(text).replace(/\%VERSION\%/mg, version);
   };
+}]).
+
+filter('rawHtml', ['$sce', function($sce){
+  return function(val) {
+    return $sce.trustAsHtml(val);
+  };
 }]);
