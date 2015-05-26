4d185582d7df0ef22bc9154c806d2e7735b10a5b
Steven Almeroth <sroth77@gmail.com>
2015-05-26 00:22:06 -0500

    xray title

--- app/js/xrays.js
+++ app/js/xrays.js
@@ -7,9 +7,13 @@
   }); // dom ready
 
   $(document).on("click", ".table-xrays img", function( jqevent ) {
-    $('#imagepreview').attr('src', this.src);
-    $('#imagepreview').
-      attr('width', Math.min(this.naturalWidth, 555)).
+
+    $('#xrayModalTitle').text(this.src.replace('http://centerstar.org/carbon-images/xrays/', ''));
+
+    $('#xrayModalPreview').
+      attr('src', this.src).
+      attr('width', Math.min(this.naturalWidth, 500)).
       on("click", function() { window.open( this.src )});
-    $('#imagemodal').modal('show');
+
+    $('#xrayDialog').modal('show');
   });
